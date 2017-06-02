484401f9880eba92c18bdb5f5b2b0be51e47ab26
keychera <kyogatama@gmail.com>
2017-06-02 15:03:25 +0700

    :package: first try Jquery animation

--- assets/js/test.js
+++ assets/js/test.js
@@ -1,6 +1,11 @@
 function main() {
     $('.card').hide();
     $('.card').fadeIn(1000);
+    $('.card').on("mouseenter", function() {
+        $(this).children('.tri-cover').animate({ bottom: "-=20" }, 200);
+    }).on("mouseleave", function() {
+        $(this).children('.tri-cover').animate({ bottom: "0" }, 200);
+    });
 }
 
 $(document).ready(main());
